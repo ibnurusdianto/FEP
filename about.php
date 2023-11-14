0560bacee4eb346ee9961d032a1c6f9d2a273636
ibnurusdianto <ibnu.rusdianto55@gmail.com>
2023-11-14 18:23:38 +0700

    modifikasi front-end

--- about.php
+++ about.php
@@ -19,17 +19,17 @@
 <!--Navbar-->
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="">FEP (Free E-Books Portal)</a>
+        <a class="navbar-brand" href="index.php">FEP (Free E-Books Portal)</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="backup-html/index.html">Home</a>
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="backup-html/about.html">About</a>
+                    <a class="nav-link" href="about.php">About</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -43,7 +43,7 @@
                     </ul>
                 </li>
             </ul>
-            <a href="backup-html/login.html" class="btn btn-success" role="button">Login</a>
+            <a href="login.php" class="btn btn-success" role="button">Login</a>
             <a href="register.html" class="btn btn-info" role="button">Register</a>
             <a href="admin.html" class="btn btn-warning" role="button">Contact Admin</a>
         </div>
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <!--Navbar-->
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="">FEP (Free E-Books Portal)</a>
+        <a class="navbar-brand" href="index.php">FEP (Free E-Books Portal)</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
